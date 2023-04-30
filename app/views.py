21928c847cb34057883999baef12fbd26c3e8ae0
MatheusMorata <matlucar09@hotmail.com>
2023-04-30 15:31:05 -0300

    Criei um formulario

--- app/views.py
+++ app/views.py
@@ -5,3 +5,10 @@ def home(request):
     template = "home.html"
     pessoas = Pessoa.objects.all()
     return render(request,template,{"pessoas":pessoas})
+
+def salvar(request):
+    template = "home.html"
+    nome_post = request.POST.get("nome")
+    Pessoa.objects.create(nome=nome_post)
+    pessoas = Pessoa.objects.all()
+    return render(request,template,{"pessoas":pessoas})
--- projeto/urls.py
+++ projeto/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import home
+from app.views import home,salvar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home),
+    path('salvar',salvar,name="salvar"),
 ]

cc9f514cacb81773a4f2d4d2b96986d239341297
MatheusMorata <matlucar09@hotmail.com>
2023-04-30 15:17:55 -0300

    Conectei o back com fron

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from app.models import Pessoa
 
 def home(request):
     template = "home.html"
-    return render(request,template)
+    pessoas = Pessoa.objects.all()
+    return render(request,template,{"pessoas":pessoas})
